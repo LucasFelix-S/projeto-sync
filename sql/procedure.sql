89b5b2b4a048ffa6901ffd2e0a7fa88e8c71bbd6
Lucas Felix Santos <started.lucasfelixsantos@gmail.com>
2026-09-22 22:02:31 -0300

    sql: transformando o script de extração do Json para uma Stored Procedure

---
+++ sql/procedure.sql
@@ -0,0 +1,87 @@
+CREATE PROCEDURE stp_ExtrairJSon
+AS
+BEGIN
+
+	DECLARE @DadosJson TABLE (
+		Id					BIGINT,
+		codigoErp			BIGINT,
+		descricao			VARCHAR(150),
+		categoriaId			INT,
+		estoque				DECIMAL(18,4),
+		preco				DECIMAL(18,2),
+		statusId			INT
+	);
+	
+	BEGIN TRY
+		BEGIN TRANSACTION;
+			
+			INSERT INTO @DadosJson(Id, codigoErp, descricao, categoriaId, estoque, preco, statusId)
+			SELECT
+				A.ID,
+				dados.codigoErp,
+				dados.descricao,
+				dados.categoriaId,
+				dados.estoque,
+				dados.preco,
+				dados.statusId
+			FROM TB_JSON_RECEBIDO A
+			CROSS APPLY OPENJSON(CONTEUDO)
+				
+			WITH (
+				codigoErp BIGINT,
+				descricao VARCHAR(150),
+				categoriaId INT,
+				estoque DECIMAL(18,4),
+				preco DECIMAL(18, 2),
+				statusId INT
+			) AS dados
+			
+			WHERE A.TIPO = 'produtos'
+			  AND A.PROCESSADO = 'N';
+			
+			INSERT INTO TB_CADASTRO_PRODUTO(CODIGO_ERP, DESCRICAO, ID_CATEGORIA, ID_STATUS)
+			SELECT
+				codigoErp,
+				descricao,
+				categoriaId,
+				statusId	
+			FROM @DadosJson;
+					
+			
+			INSERT INTO TB_ESTOQUE_PRODUTO(CODIGO_ERP, SALDO_ESTOQUE)
+			SELECT
+				codigoErp,
+				estoque	
+			FROM @DadosJson;
+			
+			
+			INSERT INTO TB_PRECO_PRODUTO(CODIGO_ERP, PRECO)
+			SELECT
+				codigoErp,
+				preco
+			FROM @DadosJson;
+			
+			
+			UPDATE A
+			SET PROCESSADO = 'S'
+			FROM TB_JSON_RECEBIDO A
+			INNER JOIN @DadosJSon B ON B.Id = A.ID;
+		
+		COMMIT TRANSACTION;
+	END TRY
+		
+		BEGIN CATCH
+		
+			IF @@TRANCOUNT > 0
+	    		ROLLBACK TRANSACTION;
+			SELECT
+			    ERROR_NUMBER() AS numero,
+			    ERROR_MESSAGE() AS mensagem;
+		
+	END CATCH
+	
+END;
+
+
+
+SELECT * FROM sys.procedures WHERE name = 'stp_ExtrairJSon'
